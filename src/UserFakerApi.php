7aa470281095824575d0dc4998f4e3ff1ca08260
zzinsidezz <peluhovskiy_sergey@ukr.net>
2020-01-28 14:23:13 +0200

    Added class UserFakerApi and some description for functionality

---
+++ src/UserFakerApi.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pelukho\userfaker\src;
+
+
+class UserFakerApi
+{
+    # https://randomuser.me/api/?nat=gb&format=json&gender=female
+
+    #curl to the api
+
+    #$ch = \curl_init();
+    #\curl_setopt($ch, CURLOPT_URL, $this->>apiUrl);
+    #\curl_setopt($ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
+    #\curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    #
+    #$res = \curl_exec($ch);
+    #
+    #\curl_close($ch);
+
+    # nationality:
+    # AU, BR, CA, CH, DE, DK, ES, FI, FR, GB, IE, IR, NO, NL, NZ, TR, US
+}
