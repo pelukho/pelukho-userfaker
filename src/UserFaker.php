23e0600093927bf6b2f5030c2c768269e248aa03
zzinsidezz <peluhovskiy_sergey@ukr.net>
2020-01-28 14:22:50 +0200

    Added class UserFakerApi and some description for functionality

---
+++ src/UserFaker.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pelukho\userfaker\src;
+
+use pelukho\userfaker\src\UserFakerApi;
+
+class UserFaker
+{
+    /**
+     * params for query
+     * - gender (male, female)
+     * - nationalities
+     * - image (large, medium, thumbnail)
+     *  - size
+     * - email
+     * - phone
+     */
+}
