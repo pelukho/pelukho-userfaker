f73e7e06e1324a67f4629b8ba2dc5aceb5107f3a
Peluhovskiy Serhii <peluhovskiy_sergey@ukr.net>
2020-01-28 23:12:46 +0200

    Added UserFakerApi functionality

--- src/UserFakerApi.php
+++ src/UserFakerApi.php
@@ -7,19 +7,65 @@ namespace pelukho\userfaker\src;
 
 class UserFakerApi
 {
-    # https://randomuser.me/api/?nat=gb&format=json&gender=female
+    private ?string $gender;
+    private ?string $nationality;
+    private $ch = null;
+    public array $userData = array();
 
-    #curl to the api
+    private const API_URL = 'https://randomuser.me/api/?format=json';
 
-    #$ch = \curl_init();
-    #\curl_setopt($ch, CURLOPT_URL, $this->>apiUrl);
-    #\curl_setopt($ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
-    #\curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    #
-    #$res = \curl_exec($ch);
-    #
-    #\curl_close($ch);
+    public function __construct(?string $gender = null, ?string $nationality = null)
+    {
+        $this->gender = $gender;
+        $this->nationality = $nationality;
+    }
 
-    # nationality:
-    # AU, BR, CA, CH, DE, DK, ES, FI, FR, GB, IE, IR, NO, NL, NZ, TR, US
+    public function getUserData(): array
+    {
+        $genderParam = $this->isValidGender() ? '&gender=' . $this->gender : '';
+        $nationalityParam = $this->isNationalityAvailable() ? '&nat=' . $this->nationality : '';
+
+        $this->ch = \curl_init();
+        \curl_setopt($this->ch, CURLOPT_URL, self::API_URL . $genderParam . $nationalityParam);
+        \curl_setopt($this->ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
+        \curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $this->userData = \json_decode(\curl_exec($this->ch), true);
+
+        \curl_close($this->ch);
+
+        return $this->userData;
+    }
+
+    private function isNationalityAvailable(): bool
+    {
+        if (\is_null($this->nationality)) return false;
+
+        return \in_array(\strtoupper($this->nationality),
+            array(
+                'AU', 'BR', 'CA', 'CH',
+                'DE', 'DK', 'ES', 'FI',
+                'FR', 'GB', 'IE', 'IR',
+                'NO', 'NL', 'NZ', 'TR', 'US'
+            )
+        );
+    }
+
+    private function isValidGender(): bool
+    {
+        if (\is_null($this->gender)) return false;
+
+        switch (\strtolower($this->gender)) {
+            case 'female':
+            case 'f':
+                $this->gender = 'female';
+                return true;
+            case 'male':
+            case 'm':
+                $this->gender = 'male';
+                return true;
+            default:
+                return false;
+        }
+    }
 }
